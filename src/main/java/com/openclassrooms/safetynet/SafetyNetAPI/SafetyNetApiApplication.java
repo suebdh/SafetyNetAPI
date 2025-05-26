8479c000cbcf81f0c0bc58fd8e82666f018ede2a
Sarar Hammar <hammar.sarar@gmail.com>
2025-05-26 23:59:58 +0200

    Initial commit: create Spring Boot project structure

---
+++ src/main/java/com/openclassrooms/safetynet/SafetyNetAPI/SafetyNetApiApplication.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.safetynet.SafetyNetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SafetyNetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/safetynet/SafetyNetAPI/SafetyNetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.safetynet.SafetyNetAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
