bf75d1f232c6221b41dcac6c41c67399970ccbee
Sarar Hammar <hammar.sarar@gmail.com>
2025-06-06 15:46:52 +0200

    feat: implement REST endpoints in PersonController for CRUD operations
    - Added GET /persons to retrieve all persons
    - Added POST /person to create a new person
    - Added PUT /person to update an existing person
    - Added DELETE /person with query params firstName and lastName to delete a person
    - All endpoints delegate business logic to PersonService
    
    Tested with Postman, all endpoints working as expected.

--- src/main/java/com/openclassrooms/safetynet/safetynetapi/controller/PersonController.java
+++ src/main/java/com/openclassrooms/safetynet/safetynetapi/controller/PersonController.java
@@ -3,8 +3,7 @@ package com.openclassrooms.safetynet.safetynetapi.controller;
 import com.openclassrooms.safetynet.safetynetapi.model.Person;
 import com.openclassrooms.safetynet.safetynetapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,4 +17,20 @@ public class PersonController {
     List<Person> getAllPersons() {
         return personService.getAllPersons();
     }
+
+    @PostMapping("/person")  // Mapper la méthode sur l’URL /person et la méthode HTTP POST
+    public Person addPerson(@RequestBody Person person) {  // Récupérer le JSON de la requête, converti en objet Person
+        return personService.save(person);  // Appeler le service pour sauvegarder la personne et Renvoyer la personne sauvegardée
+    }
+
+    @PutMapping("/person")
+    public Person updatePerson(@RequestBody Person person) {
+        return personService.update(person);
+    }
+
+    @DeleteMapping("/person")
+    public void deletePerson(@RequestParam String firstName, @RequestParam String lastName) {
+        personService.delete(firstName, lastName);
+    }
+
 }
