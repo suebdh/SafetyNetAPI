6357e6d188f8c0292f4554d29f27960b4d03b0a6
Sarar Hammar <hammar.sarar@gmail.com>
2025-06-11 05:51:23 +0200

    feat: Implement FireStationRepository interface and InMemoryFireStationRepository with CRUD operations

---
+++ src/main/java/com/openclassrooms/safetynet/safetynetapi/repository/FireStationRepository.java
@@ -0,0 +1,19 @@
+package com.openclassrooms.safetynet.safetynetapi.repository;
+
+import com.openclassrooms.safetynet.safetynetapi.model.FireStation;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FireStationRepository {
+    List<FireStation> getFirestations();
+    List<FireStation> getFirestationByStationNumber(Integer station);
+    FireStation getFirestationByAddress(String address);
+    void saveFirestation(FireStation fireStation);
+    void updateFirestation(FireStation fireStation);
+    boolean deleteFirstOccurrenceFirestationByAddress(String address);
+    boolean deleteAllFirestationByAddress(String address);
+    boolean deleteByStationNumber(int stationNumber);
+
+}
